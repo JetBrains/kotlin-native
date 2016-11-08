cba84b9ceb834ea6591a70ad993e2f79e3e2a64b
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2016-11-08 19:53:59 +0300

    Add more runtime, avoid codegen for interfaces.

---
+++ runtime/src/main/kotlin/kotlin/Nothing.kt
@@ -0,0 +1,9 @@
+package kotlin
+
+/**
+ * Nothing has no instances. You can use Nothing to represent "a value that never exists": for example,
+ * if a function has the return type of Nothing, it means that it never returns (always throws an exception).
+ */
+
+// TODO: enable, once empty classes do not crash RTTI generators.
+// public class Nothing private constructor() {}
