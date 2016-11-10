9bad54072bf8d4cfb66412b9b28357d3b9ec6296
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2016-11-10 18:31:06 +0300

    Add primitive to String conversion.

---
+++ backend.native/tests/runtime/basic/tostring0.kt
@@ -0,0 +1,9 @@
+fun main(args : Array<String>) {
+    println(30.toByte().toString())
+    println(239.toShort().toString())
+    println('A'.toString())
+    println(1122334455.toString())
+    println(112233445566778899.toString())
+    println(1e27.toFloat().toString())
+    println(1e-300.toDouble().toString())
+}
