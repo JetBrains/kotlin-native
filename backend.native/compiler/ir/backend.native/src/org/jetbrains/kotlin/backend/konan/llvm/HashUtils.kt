7989a18c1fc62d258b33c03670e515f68161825c
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2016-11-21 10:39:33 +0300

    Use alloc().

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/HashUtils.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/HashUtils.kt
@@ -24,7 +24,7 @@ internal fun globalHash(data: ByteArray, retValPlacement: Placement): GlobalHash
 internal fun base64Encode(data: ByteArray): String {
     memScoped {
         val resultSize = 4 * data.size / 3 + 3 + 1
-        val result = malloc(NativeArray of Int8Box length resultSize)
+        val result = alloc(NativeArray of Int8Box length resultSize)
         val bytes = allocNativeArrayOf(data)
         Base64Encode(bytes.ptr, data.size, result.ptr, resultSize)
         // TODO: any better way to do that without two copies?
