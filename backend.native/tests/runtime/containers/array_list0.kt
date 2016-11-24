bb4dbec7969d437a86e941e176609bc5a026b155
Konstantin Anisimov <konstantin.anisimov@jetbrains.com>
2016-11-24 18:21:41 +0300

    TEST: array_list0.kt: no boxing yet

--- backend.native/tests/runtime/containers/array_list0.kt
+++ backend.native/tests/runtime/containers/array_list0.kt
@@ -2,7 +2,7 @@ fun main(args : Array<String>) {
     val arrayList = kotlin.collections.BaseArrayList<String>(5)
     var index = 0
     while (index < arrayList.size) {
-        arrayList[index] = "element " + (++index)
+        arrayList[index] = "element " + (++index).toString()
     }
     for (it in arrayList) {
         println(it)
