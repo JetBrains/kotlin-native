0f1c2658c1299db43b580159c60756c737e20b41
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2016-12-01 15:48:10 +0700

    runtime: implement several functions in Kotlin
    * Throw{NullPointer,ArrayIndexOutOfBounds,ClassCast}Exception

---
+++ runtime/src/main/kotlin/konan/internal/RuntimeUtils.kt
@@ -0,0 +1,16 @@
+package konan.internal
+
+@ExportForCppRuntime
+internal fun ThrowNullPointerException() {
+    throw NullPointerException()
+}
+
+@ExportForCppRuntime
+internal fun ThrowArrayIndexOutOfBoundsException() {
+    throw IndexOutOfBoundsException()
+}
+
+@ExportForCppRuntime
+internal fun ThrowClassCastException() {
+    throw ClassCastException()
+}
