44c1c614bf07cefeb13583603768370ac78fc235
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2016-12-13 13:03:39 +0300

    Make lookupi algorithm selection switchable.

--- runtime/src/main/cpp/TypeInfo.cpp
+++ runtime/src/main/cpp/TypeInfo.cpp
@@ -1,31 +1,32 @@
 #include "Assert.h"
 #include "TypeInfo.h"
 
+// If one shall use binary search when looking up methods and fields.
+// TODO: maybe select strategy basing on number of elements.
+#define USE_BINARY_SEARCH 0
+
 extern "C" {
+#if USE_BINARY_SEARCH
 
 int LookupFieldOffset(const TypeInfo* info, FieldNameHash nameSignature) {
-  // TODO: make it binary search?
-  for (int i = 0; i < info->fieldsCount_; ++i) {
-    if (info->fields_[i].nameSignature_ == nameSignature) {
-      return info->fields_[i].fieldOffset_;
-    }
+  int bottom = 0;
+  int top = info->fieldsCount_ - 1;
+
+  while (bottom <= top) {
+    int middle = (bottom + top) / 2;
+    if (info->fields_[middle].nameSignature_ < nameSignature)
+      bottom = middle + 1;
+    else if (info->fields_[middle].nameSignature_ == nameSignature)
+      return info->fields_[middle].fieldOffset_;
+    else
+      top = middle - 1;
   }
+
   RuntimeAssert(false, "Unknown field");
   return -1;
 }
 
 void* LookupOpenMethod(const TypeInfo* info, MethodNameHash nameSignature) {
-  // TODO: make it binary search?
-  for (int i = 0; i < info->openMethodsCount_; ++i) {
-    if (info->openMethods_[i].nameSignature_ == nameSignature) {
-      return info->openMethods_[i].methodEntryPoint_;
-    }
-  }
-  RuntimeAssert(false, "Unknown open method");
-  return nullptr;
-}
-
-void* LookupOpenMethodBinary(const TypeInfo* info, MethodNameHash nameSignature) {
   int bottom = 0;
   int top = info->openMethodsCount_ - 1;
 
@@ -42,5 +43,29 @@ void* LookupOpenMethodBinary(const TypeInfo* info, MethodNameHash nameSignature)
   RuntimeAssert(false, "Unknown open method");
   return nullptr;
 }
+
+#else
+
+int LookupFieldOffset(const TypeInfo* info, FieldNameHash nameSignature) {
+  for (int i = 0; i < info->fieldsCount_; ++i) {
+    if (info->fields_[i].nameSignature_ == nameSignature) {
+      return info->fields_[i].fieldOffset_;
+    }
+  }
+  RuntimeAssert(false, "Unknown field");
+  return -1;
+}
+
+void* LookupOpenMethod(const TypeInfo* info, MethodNameHash nameSignature) {
+  for (int i = 0; i < info->openMethodsCount_; ++i) {
+    if (info->openMethods_[i].nameSignature_ == nameSignature) {
+      return info->openMethods_[i].methodEntryPoint_;
+    }
+  }
+  RuntimeAssert(false, "Unknown open method");
+  return nullptr;
+}
+
+#endif
 
 }
