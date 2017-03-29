cab4cfc5877f21167973974029d0e89c9c27634d
Igor Chevdar <igor.chevdar@jetbrains.com>
2017-03-29 19:15:31 +0300

    Changed type of exception from CastClassException to TypeCastException
    When converting from null to non-nullable type kotlin JVM throws TypeCastException

--- runtime/src/main/kotlin/konan/internal/RuntimeUtils.kt
+++ runtime/src/main/kotlin/konan/internal/RuntimeUtils.kt
@@ -15,6 +15,10 @@ fun ThrowClassCastException(): Nothing {
     throw ClassCastException()
 }
 
+fun ThrowTypeCastException(): Nothing {
+    throw TypeCastException()
+}
+
 @ExportForCppRuntime
 internal fun ThrowArithmeticException() : Nothing {
     throw ArithmeticException()
