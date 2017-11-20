4f472e023024140754a554fe87098c4e524e2fc3
Roman Elizarov <elizarov@gmail.com>
2017-11-20 15:33:19 +0300

    Better documentation on Disposable protocol

--- samples/videoplayer/src/main/kotlin/Disposable.kt
+++ samples/videoplayer/src/main/kotlin/Disposable.kt
@@ -1,19 +1,35 @@
 
 import kotlinx.cinterop.*
 
+/**
+ * Disposable class manages and owns all its native resources. It allocates some resources
+ * during construction and may allocate some additional resources during operation.
+ * It must free all its resource once [dispose] is invoked. "Disposed" is a final state of the disposable
+ * class. It is not supposed to be used after being disposed.
+ */
 interface Disposable {
+    /**
+     * Disposes all native resources owned by this class. This function must be invoked
+     * exactly once as the last operation on the corresponding class.
+     */
     fun dispose()
 }
 
+/**
+ * Helper class to implement [Disposable] interface. It contains an [arena] for native
+ * memory allocations and a number of helper methods to simplify management of other
+ * kinds of native resources.
+ *
+ * It is important to wrap all potentially exception-throwing code in the class constructor
+ * into [tryConstruct] invocation, because, when object construction fails with exception,
+ * it ensures that all the resource that were allocated so far will get freed.
+ */
 abstract class DisposableContainer : Disposable {
     val arena = Arena()
     
     override fun dispose() {
         arena.clear()
     }
-
-    // These functions should be used in constructor only.
-    // They will properly dispose of partially initialized object on failure
 
     inline fun <T> tryConstruct(init: () -> T): T =
         try { init() }
