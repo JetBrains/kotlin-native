c1bd2646a4d6f84bbc706e2fef956d70e0490ae8
Ilya Matveev <ilya.matveev@jetbrains.com>
2018-02-08 15:24:22 +0700

    [samples] Fix GTK command-line build

--- samples/gtk/build.sh
+++ samples/gtk/build.sh
@@ -22,9 +22,10 @@ mkdir -p $DIR/build/c_interop/
 mkdir -p $DIR/build/bin/
 
 echo "Generating GTK stubs, may take few mins depending on the hardware..."
-cinterop -J-Xmx8g -compilerOpts "$IPREFIX/atk-1.0 $IPREFIX/gdk-pixbuf-2.0 $IPREFIX/cairo $IPREFIX/pango-1.0 \
-         -I/opt/local/lib/glib-2.0/include $IPREFIX/gtk-3.0 $IPREFIX/glib-2.0" \
-	 -def $DIR/src/main/c_interop/gtk3.def -target $TARGET -o $DIR/build/c_interop/gtk3 || exit 1
+cinterop -J-Xmx8g -copt $IPREFIX/atk-1.0 -copt $IPREFIX/gdk-pixbuf-2.0 -copt $IPREFIX/cairo -copt $IPREFIX/pango-1.0 \
+         -copt -I/opt/local/lib/glib-2.0/include -copt -I/usr/lib/x86_64-linux-gnu/glib-2.0/include \
+         -copt $IPREFIX/gtk-3.0 -copt $IPREFIX/glib-2.0 -def $DIR/src/main/c_interop/gtk3.def \
+         -target $TARGET -o $DIR/build/c_interop/gtk3 || exit 1
 
 konanc -target $TARGET $DIR/src/main/kotlin -library $DIR/build/c_interop/gtk3 \
        -o $DIR/build/bin/Gtk3Demo || exit 1
