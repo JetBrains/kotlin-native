6c45c644ed07af9d5ccb16c043d5df4d47c722d4
Nikolay Igotti <igotti@gmail.com>
2018-03-12 16:08:36 +0100

    Typed arrays API

---
+++ backend.native/tests/runtime/collections/typed_array0.kt
@@ -0,0 +1,18 @@
+package runtime.collections.typed_array0
+
+import kotlin.test.*
+
+@Test fun runTest() {
+    // Those tests assume little endian bit ordering.
+    val array = ByteArray(42)
+    array.setLongAt(5, 0x1234_5678_9abc_def0)
+
+    expect(0xdef0.toInt()) { array.charAt(5).toInt() }
+    expect(0x9abc.toShort()) { array.shortAt(7) }
+    expect(0x1234_5678) { array.intAt(9) }
+    try {
+        println(array.longAt(39))
+    } catch (e: ArrayIndexOutOfBoundsException) {
+        println("OK")
+    }
+}
