072b7713a06864e54c5683097be66ea3dd10403e
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2018-04-26 14:12:26 +0300

    Added IrDeadCode.kt

---
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/optimizations/IrDeadCode.kt
@@ -0,0 +1,86 @@
+package org.jetbrains.kotlin.backend.konan.optimizations
+
+import org.jetbrains.kotlin.backend.common.DeclarationContainerLoweringPass
+import org.jetbrains.kotlin.backend.common.lower.IrBuildingTransformer
+import org.jetbrains.kotlin.backend.common.runOnFilePostfix
+import org.jetbrains.kotlin.backend.konan.Context
+import org.jetbrains.kotlin.backend.konan.lower.PostInlineLowering
+import org.jetbrains.kotlin.ir.declarations.*
+import org.jetbrains.kotlin.ir.symbols.IrSymbol
+import org.jetbrains.kotlin.ir.util.transformFlat
+import org.jetbrains.kotlin.ir.visitors.IrElementTransformerVoid
+import org.jetbrains.kotlin.ir.visitors.transformChildrenVoid
+import org.jetbrains.kotlin.backend.konan.irasdescriptors.*
+import org.jetbrains.kotlin.descriptors.ClassKind
+import org.jetbrains.kotlin.konan.target.CompilerOutputKind
+
+val dceNotNeeded = mutableSetOf<IrDeclaration>()
+
+internal class IrDeadCode(val context: Context, val moduleDFG: ModuleDFG, val callGraph: CallGraph):  DeclarationContainerLoweringPass, IrElementTransformerVoid() {
+
+    val neededCallGraphNodes: MutableSet<DataFlowIR.FunctionSymbol> by lazy {
+        val result = mutableSetOf<DataFlowIR.FunctionSymbol>()
+        callGraph.directEdges.forEach {
+            val source = it.key
+            result.add(source)
+            it.value.callSites.forEach {
+                val callee = it.actualCallee
+                println("${source.name} -> ${callee.name} ${if (it.isVirtual) "[label=\"virtual\"]" else ""}; ")
+                result.add(callee)
+            }
+        }
+        result
+    }
+
+
+    fun dceNeededFunction(declaration: IrFunction): Boolean {
+        // A workaround for a CallGraph bug.
+        //if (declaration.parent is IrClass && (declaration.parent as IrClass).kind == ClassKind.OBJECT && declaration is IrConstructor) {
+
+        //  return true
+        //}
+
+        val functionMap: MutableMap<DeclarationDescriptor, DataFlowIR.FunctionSymbol> =  moduleDFG.symbolTable.functionMap
+        val dfgSymbol = functionMap[declaration]
+        if (dfgSymbol== null) return true
+        if (neededCallGraphNodes.contains(dfgSymbol)) {
+            return true
+        } else {
+            println("### dceNotNeeded: ${declaration.descriptor} $declaration")
+            dceNotNeeded.add(declaration)
+            return false
+        }
+    }
+    /*
+    fun dceNeededClass(declaration: IrClass): Boolean {
+        return if (declaration.declarations.all{(it !is IrFunction) || dceNeededFunction(it)}) true
+               else false
+    }
+    */
+
+    fun dceNeeded(declaration: IrDeclaration): Boolean {
+        return when (declaration) {
+            is IrFunction ->  dceNeededFunction(declaration)
+            /*
+            is IrClass -> true
+            */
+            else -> true
+        }
+    }
+
+        override fun lower(irDeclarationContainer: IrDeclarationContainer) {
+        irDeclarationContainer.declarations.retainAll{
+/*
+            val needed = dceNeeded(it)
+            if (!needed) dceNotNeeded.add(it)
+            needed*/
+            dceNeeded(it)
+        }
+    }
+
+    fun run() {
+        if (context.config.produce != CompilerOutputKind.PROGRAM) return
+        context.irModule!!.files.forEach{runOnFilePostfix(it)}
+        //context.irModule!!.files.forEach{lower(it)}
+    }
+}
