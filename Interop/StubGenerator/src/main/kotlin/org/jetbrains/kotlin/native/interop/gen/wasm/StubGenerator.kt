3eb4e513777d0187f48add1df9d6f6e26d14987a
Florian Friedrich <friedrich@hooster.de>
2018-05-23 18:43:09 +0200

    Implement idlInterfaceRef as function argument

--- Interop/StubGenerator/src/main/kotlin/org/jetbrains/kotlin/native/interop/gen/wasm/StubGenerator.kt
+++ Interop/StubGenerator/src/main/kotlin/org/jetbrains/kotlin/native/interop/gen/wasm/StubGenerator.kt
@@ -31,7 +31,7 @@ fun Arg.wasmMapping(): String = when (type) {
     is idlString -> "stringPointer($name), stringLengthBytes($name)"
     is idlObject -> TODO("implement me")
     is idlFunction -> "wrapFunction<R$name>($name), ArenaManager.currentArena"
-    is idlInterfaceRef -> TODO("Implement me")
+    is idlInterfaceRef -> "${name}.arena, ${name}.index"
     else -> error("Unexpected type")
 }
 
@@ -58,7 +58,7 @@ fun Arg.wasmArgNames(): List<String> = when (type) {
     is idlString -> listOf("${name}Ptr", "${name}Len")
     is idlObject -> TODO("implement me (idlObject)")
     is idlFunction -> listOf("${name}Index", "${name}ResultArena")
-    is idlInterfaceRef -> TODO("Implement me (idlInterfaceRef)")
+    is idlInterfaceRef -> listOf("${name}Arena", "${name}Index")
     else -> error("Unexpected type")
 }
 
@@ -291,7 +291,8 @@ fun Arg.composeWasmArgs(): String = when (type) {
     is idlFunction ->
         "    var $name = konan_dependencies.env.Konan_js_wrapLambda(lambdaResultArena, ${name}Index);\n"
 
-    is idlInterfaceRef -> TODO("Implement me")
+    is idlInterfaceRef -> 
+        "    var $name = kotlinObject(${name}Arena, ${name}Index);\n"
     else -> error("Unexpected type")
 }
 
