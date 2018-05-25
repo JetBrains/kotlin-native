f1fa6cc1dd38b5ffb84e4de5c3b9f3e624ac133c
Florian Friedrich <friedrich@hooster.de>
2018-05-25 23:50:22 +0200

    Add more parts to dom.idl

--- Interop/StubGenerator/src/main/kotlin/org/jetbrains/kotlin/native/interop/gen/wasm/idl/dom.kt
+++ Interop/StubGenerator/src/main/kotlin/org/jetbrains/kotlin/native/interop/gen/wasm/idl/dom.kt
@@ -14,8 +14,14 @@ val idlDom = listOf(
         Operation("fillRect", idlVoid, Arg("x", idlInt), Arg("y", idlInt), Arg("width", idlInt), Arg("height", idlInt)),
         Operation("fillText", idlVoid, Arg("test", idlString), Arg("x", idlInt),  Arg("y", idlInt), Arg("maxWidth", idlInt)),
         Operation("fill", idlVoid),
-        Operation("closePath", idlVoid)
-    ),
+        Operation("closePath", idlVoid),
+        Operation("rect", idlVoid, Arg("x", idlInt), Arg("y", idlInt), Arg("width", idlInt), Arg("height", idlInt)),
+        Operation("translate", idlVoid, Arg("x", idlInt), Arg("y", idlInt)),
+        Operation("setTransform", idlVoid, Arg("a", idlInt), Arg("b", idlInt), Arg("c", idlInt), Arg("d", idlInt), Arg("e", idlInt), Arg("f", idlInt)),
+        Operation("save", idlVoid),
+        Operation("restore", idlVoid),
+        Operation("measureText", idlInterfaceRef("TextMetrics"), Arg("text", idlString)),
+   ),
     Interface("DOMRect",
         Attribute("left", idlInt),
         Attribute("right", idlInt),
@@ -27,11 +33,21 @@ val idlDom = listOf(
         Operation("getBoundingClientRect", idlInterfaceRef("DOMRect"))
     ),
     Interface("Document",
-        Operation("getElementById", idlObject, Arg("id", idlString))
+        Operation("getElementById", idlObject, Arg("id", idlString)),
+        Operation("createElement", idlObject, Arg("tagName", idlString))
+    ),
+    Interface("HTMLImage",
+        Attribute("src", idlString)
     ),
     Interface("MouseEvent",
         Attribute("clientX", idlInt, readOnly = true),
         Attribute("clientY", idlInt, readOnly = true)
+    ),
+    Interface("KeyboardEvent",
+        Attribute("key", idlString, readOnly = true)
+    ),
+    Interface("TextMetrics",
+        Attribute("width", idlDouble, readOnly = true)
     ),
     Interface("Response",
         Operation("json", idlObject)
@@ -43,7 +59,8 @@ val idlDom = listOf(
         Attribute("document", idlInterfaceRef("Document"), readOnly = true),
 
         Operation("fetch", idlInterfaceRef("Promise"), Arg("url", idlString)),
-        Operation("setInterval", idlVoid, Arg("lambda", idlFunction), Arg("interval", idlInt))
+        Operation("setInterval", idlInt, Arg("lambda", idlFunction), Arg("interval", idlInt)),
+        Operation("clearInterval", idlVoid, Arg("intervalID", idlInt))
     )
 )
 
