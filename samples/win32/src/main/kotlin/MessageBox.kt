5a900eb2d73f0487f70d3a585ab0e3767e3ee7ea
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2018-09-06 19:32:12 +0300

    Improve Win32 APIs.

--- samples/win32/src/main/kotlin/MessageBox.kt
+++ samples/win32/src/main/kotlin/MessageBox.kt
@@ -2,6 +2,12 @@ import kotlinx.cinterop.*
 import platform.windows.*
 
 fun main(args: Array<String>) {
-    MessageBoxW(null, "Konan говорит:\nЗДРАВСТВУЙ МИР!\n",
+    val icon = memScoped { 
+      LoadImageW(null, "icon.bmp".wcstr.ptr, IMAGE_BITMAP, 128, 128, LR_LOADFROMFILE) 
+    }
+    if (icon != null) {
+      SetClassLongPtrW(GetActiveWindow(), GCLP_HICON, icon.toLong())
+    }
+    MessageBoxW(null, "Konan говорит:\nЗДРАВСТВУЙ МИР!\nИконка $icon",
             "Заголовок окна", (MB_YESNOCANCEL or MB_ICONQUESTION).convert())
 }
