6c30f22e8b802a2f961d57650f715f27301ad673
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2018-09-07 16:22:14 +0300

    Simplify

--- samples/win32/src/main/kotlin/MessageBox.kt
+++ samples/win32/src/main/kotlin/MessageBox.kt
@@ -2,15 +2,13 @@ import kotlinx.cinterop.*
 import platform.windows.*
 
 fun main(args: Array<String>) {
-    val icon = memScoped {
-      val buffer = alloc<UShortVar>(256)
-      GetModuleFileNameW(null, buffer.ptr, 256)
-      val dir = buffer.toKString()
-      LoadImageW(null, "$dir\\..\\..\\..\\icon.bmp".wcstr.ptr, IMAGE_BITMAP, 128, 128, LR_LOADFROMFILE)
+    var message = StringBuilder()
+    memScoped {
+      val buffer = allocArray<UShortVar>(MAX_PATH)
+      GetModuleFileNameW(null, buffer, MAX_PATH)
+      val path = buffer.toKString().split("\\").dropLast(1).joinToString("\\")
+      message.append("Я нахожусь в $path\n")
     }
-    if (icon != null) {
-      SetClassLongPtrW(GetActiveWindow(), GCLP_HICON, icon.toLong())
-    }
-    MessageBoxW(null, "Konan говорит:\nЗДРАВСТВУЙ МИР!\nИконка $icon",
+    MessageBoxW(null, "Konan говорит:\nЗДРАВСТВУЙ МИР!\n$message",
             "Заголовок окна", (MB_YESNOCANCEL or MB_ICONQUESTION).convert())
 }
