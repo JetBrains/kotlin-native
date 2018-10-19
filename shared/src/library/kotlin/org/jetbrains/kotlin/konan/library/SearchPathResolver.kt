66d83aba8cb67e7f883cc66a7ebd1773932832ae
alexander-gorshenev <alexander.gorshenev@jetbrains.com>
2018-10-19 12:04:06 +0300

    1.0.0 compatibility fine tune (#2227)

--- shared/src/library/kotlin/org/jetbrains/kotlin/konan/library/SearchPathResolver.kt
+++ shared/src/library/kotlin/org/jetbrains/kotlin/konan/library/SearchPathResolver.kt
@@ -208,17 +208,23 @@ internal fun SearchPathResolverWithTarget.libraryMatch(candidate: KonanLibraryIm
         return false
     }
 
-     if (candidateCompilerVersion == null ||
-        knownCompilerVersions != null &&
-            knownCompilerVersions!!.none { it.compatible(candidateCompilerVersion) } ) {
-        logger("skipping $candidatePath. The compiler versions don't match. Expected '${knownCompilerVersions?.map { it.toString(false, false) }}', found '${candidateCompilerVersion?.toString(true, true)}'")
-        return false
-    }
+    val abiVersionMatch = candidateAbiVersion != null &&
+            knownAbiVersions != null &&
+            knownAbiVersions!!.contains(candidateAbiVersion)
 
-    if (candidateAbiVersion == null ||
-        knownAbiVersions != null &&
-            !knownAbiVersions!!.contains(candidateAbiVersion)) {
+    val compilerVersionMatch = candidateCompilerVersion != null &&
+            knownCompilerVersions != null &&
+            knownCompilerVersions!!.any { it.compatible(candidateCompilerVersion) }
+
+    if (!abiVersionMatch && !compilerVersionMatch) {
         logger("skipping $candidatePath. The abi versions don't match. Expected '${knownAbiVersions}', found '${candidateAbiVersion}'")
+
+        if (knownCompilerVersions != null) {
+            val expected = knownCompilerVersions?.map { it.toString(false, false) }
+            val found = candidateCompilerVersion?.toString(true, true)
+            logger("The compiler versions don't match either. Expected '${expected}', found '${found}'")
+        }
+
         return false
     }
 
--- shared/src/main/kotlin/org/jetbrains/kotlin/konan/KonanAbiVersion.kt
+++ shared/src/main/kotlin/org/jetbrains/kotlin/konan/KonanAbiVersion.kt
@@ -6,7 +6,7 @@ fun String.parseKonanAbiVersion(): KonanAbiVersion {
 
 data class KonanAbiVersion(val version: Int) {
     companion object {
-        val CURRENT = KonanAbiVersion(1)
+        val CURRENT = KonanAbiVersion(2)
     }
     override fun toString() = "$version"
 }
