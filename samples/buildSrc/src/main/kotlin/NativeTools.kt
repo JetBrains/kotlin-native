56533a9ea6085867f971f0a155eb25533e664407
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2018-10-19 20:30:04 +0700

    inline JS-interop tasks into 'html5canvas' build.gradle file

--- samples/buildSrc/src/main/kotlin/NativeTools.kt
+++ samples/buildSrc/src/main/kotlin/NativeTools.kt
@@ -1,8 +1,5 @@
 @file:JvmName("NativeTools")
 
-import org.gradle.api.Project
-import org.gradle.api.Task
-import org.gradle.api.tasks.Exec
 import java.nio.file.Paths
 
 /*
@@ -12,23 +9,3 @@ import java.nio.file.Paths
 // Needed purely for test purposes:
 @get:JvmName("testMavenRepoUrl")
 val testMavenRepoUrl by lazy { "file://" + Paths.get(userHome, ".m2-kotlin-native-samples") }
-
-// This will be dropped after full support of JS-interop.
-// Warning: May throw exception if no property was found!
-fun Project.getJsInteropKlibFileName(packageName: String) =
-        Paths.get(buildDir.toString(), "klib", "$packageName-jsinterop.klib").toString()
-
-fun Project.createJsInteropTask(
-        name: String,
-        packageName: String
-): Task {
-    val task = tasks.create(name, Exec::class.java)
-    task.workingDir = projectDir
-    task.commandLine(
-            Paths.get(properties["org.jetbrains.kotlin.native.home"] as String, "bin", "jsinterop").toString(),
-            "-pkg", packageName,
-            "-o", getJsInteropKlibFileName(packageName),
-            "-target", "wasm32"
-    )
-    return task
-}
