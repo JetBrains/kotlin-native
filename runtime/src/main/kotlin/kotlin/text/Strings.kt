d8ae58855dbec23a60b17ddbe07779f4511f2172
Pavel Punegov <pavel.punegov@jetbrains.com>
2018-11-01 01:11:58 +0300

    Fix regionMatch for CharSequence
    (cherry picked from commit b4da3e91c9f5d3afbdd66213f86c82099a5997f9)

--- runtime/src/main/kotlin/kotlin/text/Strings.kt
+++ runtime/src/main/kotlin/kotlin/text/Strings.kt
@@ -8,12 +8,195 @@ package kotlin.text
 import kotlin.comparisons.*
 
 /**
+ * Returns the index within this string of the first occurrence of the specified character, starting from the specified offset.
+ */
+@SymbolName("Kotlin_String_indexOfChar")
+external internal actual inline fun String.nativeIndexOf(ch: Char, fromIndex: Int): Int
+
+/**
+ * Returns the index within this string of the first occurrence of the specified substring, starting from the specified offset.
+ */
+@SymbolName("Kotlin_String_indexOfString")
+external internal actual fun String.nativeIndexOf(str: String, fromIndex: Int): Int
+
+/**
+ * Returns the index within this string of the last occurrence of the specified character.
+ */
+@SymbolName("Kotlin_String_lastIndexOfChar")
+external internal actual inline fun String.nativeLastIndexOf(ch: Char, fromIndex: Int): Int
+
+/**
+ * Returns the index within this string of the last occurrence of the specified character, starting from the specified offset.
+ */
+@SymbolName("Kotlin_String_lastIndexOfString")
+external internal actual fun String.nativeLastIndexOf(str: String, fromIndex: Int): Int
+
+/**
+ * Returns `true` if this string is equal to [other], optionally ignoring character case.
+ *
+ * @param ignoreCase `true` to ignore character case when comparing strings. By default `false`.
+ */
+public actual fun String?.equals(other: String?, ignoreCase: Boolean): Boolean {
+    if (this === null)
+        return other === null
+    if (other === null)
+        return false
+    return if (!ignoreCase)
+        this.equals(other)
+    else
+        stringEqualsIgnoreCase(this, other)
+}
+
+@SymbolName("Kotlin_String_equalsIgnoreCase")
+external internal fun stringEqualsIgnoreCase(thiz: String, other: String): Boolean
+
+/**
+ * Returns a new string with all occurrences of [oldChar] replaced with [newChar].
+ */
+@SymbolName("Kotlin_String_replace")
+external public actual fun String.replace(
+        oldChar: Char, newChar: Char, ignoreCase: Boolean): String
+
+
+/**
+ * Returns a new string obtained by replacing all occurrences of the [oldValue] substring in this string
+ * with the specified [newValue] string.
+ */
+public actual fun String.replace(oldValue: String, newValue: String, ignoreCase: Boolean): String =
+        splitToSequence(oldValue, ignoreCase = ignoreCase).joinToString(separator = newValue)
+
+/**
+ * Returns a new string with the first occurrence of [oldChar] replaced with [newChar].
+ */
+public actual fun String.replaceFirst(oldChar: Char, newChar: Char, ignoreCase: Boolean): String {
+    val index = indexOf(oldChar, ignoreCase = ignoreCase)
+    return if (index < 0) this else this.replaceRange(index, index + 1, newChar.toString())
+}
+
+/**
+ * Returns a new string obtained by replacing the first occurrence of the [oldValue] substring in this string
+ * with the specified [newValue] string.
+ */
+public actual fun String.replaceFirst(oldValue: String, newValue: String, ignoreCase: Boolean): String {
+    val index = indexOf(oldValue, ignoreCase = ignoreCase)
+    return if (index < 0) this else this.replaceRange(index, index + oldValue.length, newValue)
+}
+
+/**
+ * Returns a copy of this string having its first letter lowercased, or the original string,
+ * if it's empty or already starts with a lower case letter.
+ *
+ * @sample samples.text.Strings.decapitalize
+ */
+public actual fun String.decapitalize(): String {
+    return if (isNotEmpty() && this[0].isUpperCase()) substring(0, 1).toLowerCase() + substring(1) else this
+}
+
+/**
+ * Returns `true` if this string is empty or consists solely of whitespace characters.
+ */
+public actual fun CharSequence.isBlank(): Boolean = length == 0 || indices.all { this[it].isWhitespace() }
+
+/**
+ * Returns the substring of this string starting at the [startIndex] and ending right before the [endIndex].
+ *
+ * @param startIndex the start index (inclusive).
+ * @param endIndex the end index (exclusive).
+ */
+@kotlin.internal.InlineOnly
+public actual inline fun String.substring(startIndex: Int, endIndex: Int): String =
+        subSequence(startIndex, endIndex) as String
+
+/**
+ * Returns a substring of this string that starts at the specified [startIndex] and continues to the end of the string.
+ */
+@kotlin.internal.InlineOnly
+public actual inline fun String.substring(startIndex: Int): String =
+        subSequence(startIndex, this.length) as String
+
+/**
+ * Returns `true` if this string starts with the specified prefix.
+ */
+public fun String.startsWith(prefix: String, ignoreCase: Boolean = false): Boolean =
+        regionMatches(0, prefix, 0, prefix.length, ignoreCase)
+
+/**
+ * Returns `true` if a substring of this string starting at the specified offset [startIndex] starts with the specified prefix.
+ */
+public fun String.startsWith(prefix: String, startIndex: Int, ignoreCase: Boolean = false): Boolean =
+        regionMatches(startIndex, prefix, 0, prefix.length, ignoreCase)
+
+/**
+ * Returns `true` if this string ends with the specified suffix.
+ */
+public fun String.endsWith(suffix: String, ignoreCase: Boolean = false): Boolean =
+        regionMatches(length - suffix.length, suffix, 0, suffix.length, ignoreCase)
+
+/**
+ * Returns `true` if the specified range in this char sequence is equal to the specified range in another char sequence.
+ * @param thisOffset the start offset in this char sequence of the substring to compare.
+ * @param other the string against a substring of which the comparison is performed.
+ * @param otherOffset the start offset in the other char sequence of the substring to compare.
+ * @param length the length of the substring to compare.
+ */
+public actual fun CharSequence.regionMatches(
+        thisOffset: Int, other: CharSequence, otherOffset: Int, length: Int,
+        ignoreCase: Boolean): Boolean {
+    return if (this is String && other is String) {
+        this.regionMatches(thisOffset, other, otherOffset, length, ignoreCase)
+    } else {
+        regionMatchesImpl(thisOffset, other, otherOffset, length, ignoreCase)
+    }
+}
+
+/**
+ * Returns `true` if the specified range in this string is equal to the specified range in another string.
+ * @param thisOffset the start offset in this string of the substring to compare.
+ * @param other the string against a substring of which the comparison is performed.
+ * @param otherOffset the start offset in the other string of the substring to compare.
+ * @param length the length of the substring to compare.
+ */
+@SymbolName("Kotlin_String_regionMatches")
+external public fun String.regionMatches(
+        thisOffset: Int, other: String, otherOffset: Int, length: Int,
+        ignoreCase: Boolean = false): Boolean
+
+/**
+ * Returns a copy of this string converted to upper case using the rules of the default locale.
+ */
+@SymbolName("Kotlin_String_toUpperCase")
+external public actual fun String.toUpperCase(): String
+
+/**
+ * Returns a copy of this string converted to lower case using the rules of the default locale.
+ */
+@SymbolName("Kotlin_String_toLowerCase")
+@Suppress("NOTHING_TO_INLINE")
+external public actual inline fun String.toLowerCase(): String
+
+/**
+ * Returns a new character array containing the characters from this string.
+ */
+@SymbolName("Kotlin_String_toCharArray")
+external public fun String.toCharArray(): CharArray
+
+/**
+ * Returns a copy of this string having its first letter uppercased, or the original string,
+ * if it's empty or already starts with an upper case letter.
+ *
+ * @sample samples.text.Strings.capitalize
+ */
+public actual fun String.capitalize(): String {
+    return if (isNotEmpty() && this[0].isLowerCase()) substring(0, 1).toUpperCase() + substring(1) else this
+}
+
+/**
  * Returns a string containing this char sequence repeated [n] times.
  * @throws [IllegalArgumentException] when n < 0.
  * @sample samples.text.Strings.repeat
  */
 public actual fun CharSequence.repeat(n: Int): String {
-    require (n >= 0) { "Count 'n' must be non-negative, but was $n." }
+    require(n >= 0) { "Count 'n' must be non-negative, but was $n." }
 
     return when (n) {
         0 -> ""
@@ -45,7 +228,7 @@ public actual fun String(chars: CharArray): String = fromCharArray(chars, 0, cha
 public actual fun String(chars: CharArray, offset: Int, length: Int): String = fromCharArray(chars, offset, length)
 
 @SymbolName("Kotlin_String_compareToIgnoreCase")
-external fun compareToIgnoreCase(thiz:String, other:String):Int
+external fun compareToIgnoreCase(thiz: String, other: String): Int
 
 actual fun String.compareTo(other: String, ignoreCase: Boolean): Int {
     return if (!ignoreCase) this.compareTo(other)
@@ -55,4 +238,4 @@ actual fun String.compareTo(other: String, ignoreCase: Boolean): Int {
 private val STRING_CASE_INSENSITIVE_ORDER = Comparator<String> { a, b -> a.compareTo(b, ignoreCase = true) }
 
 actual val String.Companion.CASE_INSENSITIVE_ORDER: Comparator<String>
-  get() = STRING_CASE_INSENSITIVE_ORDER
+    get() = STRING_CASE_INSENSITIVE_ORDER
