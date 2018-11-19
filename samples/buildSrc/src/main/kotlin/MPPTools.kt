c61b62dbc32b23ba4e4f5783afad96ac9c641b3c
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2018-11-19 10:43:49 +0700

    Samples: libcurl for Windows with static linking

--- samples/buildSrc/src/main/kotlin/MPPTools.kt
+++ samples/buildSrc/src/main/kotlin/MPPTools.kt
@@ -27,6 +27,9 @@ val isWindows by lazy { hostOs.startsWith("Windows") }
 val isLinux by lazy { hostOs == "Linux" }
 
 // Short-cuts for mostly used paths.
+@get:JvmName("msysPath")
+val msysPath by lazy { System.getenv("MSYS2_DIR") ?: "c:/msys64" }
+
 @get:JvmName("mingwPath")
 val mingwPath by lazy { System.getenv("MINGW64_DIR") ?: "c:/msys64/mingw64" }
 
