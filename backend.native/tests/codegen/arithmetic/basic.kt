f12617d9c273aeb941cd6ac5e74bb7ce57d2d588
Sergey Bogolepov <ace.is.not.my.card@gmail.com>
2018-12-13 00:29:47 +0700

    fix tests

--- backend.native/tests/codegen/arithmetic/basic.kt
+++ backend.native/tests/codegen/arithmetic/basic.kt
@@ -47,13 +47,6 @@ fun uintTests() {
 }
 
 @Test
-fun divisionByZero() {
-    assertFailsWith(ArithmeticException::class, { 5 / 0 })
-    assertEquals(1, 5 / try { 0 / 0; 1 } catch (e: ArithmeticException) { 5 })
-    assertEquals(Double.NaN, 0.0 / 0.0)
-}
-
-@Test
 fun charConversions() {
     assertEquals(97.0, 'a'.toDouble())
     assertEquals(-1, Char.MAX_VALUE.toShort())
