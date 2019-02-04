d3a4e03e50d45e71a1f7bff8d3fa9f1307973f33
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2019-02-04 15:33:18 +0300

    Bump ABI version.

--- shared/src/main/kotlin/org/jetbrains/kotlin/konan/KonanAbiVersion.kt
+++ shared/src/main/kotlin/org/jetbrains/kotlin/konan/KonanAbiVersion.kt
@@ -6,7 +6,7 @@ fun String.parseKonanAbiVersion(): KonanAbiVersion {
 
 data class KonanAbiVersion(val version: Int) {
     companion object {
-        val CURRENT = KonanAbiVersion(6)
+        val CURRENT = KonanAbiVersion(7)
     }
     override fun toString() = "$version"
 }
