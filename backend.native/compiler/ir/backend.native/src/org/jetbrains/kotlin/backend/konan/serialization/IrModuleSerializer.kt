3352055743f91e99d67b5985999c52d65ef82b47
Igor Chevdar <igor.chevdar@jetbrains.com>
2019-02-20 13:35:47 +0500

    Review fix

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/IrModuleSerializer.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/IrModuleSerializer.kt
@@ -1080,9 +1080,10 @@ internal class IrModuleSerializer(
         }
 
         // Make sure that all top level properties are initialized on library's load.
-        file.declarations.filterIsInstance<IrProperty>().forEach {
-            proto.addExplicitlyExportedToCompiler(serializeIrSymbol(it.getter!!.symbol))
-        }
+        file.declarations
+                .filterIsInstance<IrProperty>()
+                .filter { it.backingField?.initializer != null }
+                .forEach { proto.addExplicitlyExportedToCompiler(serializeIrSymbol(it.backingField!!.symbol)) }
 
         file.acceptVoid(object: IrElementVisitorVoid {
             override fun visitElement(element: IrElement) {
