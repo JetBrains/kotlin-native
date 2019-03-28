aaad80616a27817e9951b6e143ae8cbd18aee7a7
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2019-03-28 12:53:30 +0300

    Fixes

--- tools/kotlin-native-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/konan/KonanPlugin.kt
+++ tools/kotlin-native-gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/gradle/plugin/konan/KonanPlugin.kt
@@ -203,13 +203,6 @@ internal fun MutableList<String>.addFileArgs(parameter: String, values: Collecti
     }
 }
 
-internal fun MutableList<String>.addListArg(parameter: String, values: List<String>) {
-    if (values.isNotEmpty()) {
-        addArg(parameter, values.joinToString(separator = " "))
-        println("$parameter, ${values.map { "\"$it\"" }.joinToString(separator = " ")}")
-    }
-}
-
 // endregion
 
 internal fun dumpProperties(task: Task) {
