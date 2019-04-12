e70d499f616ce932f512d01e31d2a079864ab165
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2019-04-12 17:17:47 +0300

    Support variadic Objective-C methods

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/lower/InteropLowering.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/lower/InteropLowering.kt
@@ -597,7 +597,7 @@ internal class InteropLoweringPart1(val context: Context) : BaseInteropIrTransfo
                     initMethodInfo,
                     superQualifier = symbolTable.referenceClass(expression.descriptor.constructedClass),
                     receiver = builder.getRawPtr(builder.irGet(constructedClass.thisReceiver!!)),
-                    arguments = initMethod.valueParameters.map { expression.getValueArgument(it.index)!! },
+                    arguments = initMethod.valueParameters.map { expression.getValueArgument(it.index) },
                     call = expression,
                     method = initMethod
             )
@@ -631,7 +631,7 @@ internal class InteropLoweringPart1(val context: Context) : BaseInteropIrTransfo
             info: ObjCMethodInfo,
             superQualifier: IrClassSymbol?,
             receiver: IrExpression,
-            arguments: List<IrExpression>,
+            arguments: List<IrExpression?>,
             call: IrFunctionAccessExpression,
             method: IrSimpleFunction
     ): IrExpression = generateWithStubs(call) {
@@ -657,7 +657,7 @@ internal class InteropLoweringPart1(val context: Context) : BaseInteropIrTransfo
             val initMethod = callee.getObjCInitMethod()
 
             if (initMethod != null) {
-                val arguments = descriptor.valueParameters.map { expression.getValueArgument(it)!! }
+                val arguments = descriptor.valueParameters.map { expression.getValueArgument(it) }
                 assert(expression.extensionReceiver == null)
                 assert(expression.dispatchReceiver == null)
 
@@ -672,7 +672,7 @@ internal class InteropLoweringPart1(val context: Context) : BaseInteropIrTransfo
 
         descriptor.getObjCFactoryInitMethodInfo()?.let { initMethodInfo ->
             val arguments = (0 until expression.valueArgumentsCount)
-                    .map { index -> expression.getValueArgument(index)!! }
+                    .map { index -> expression.getValueArgument(index) }
 
             return builder.at(expression).run {
                 val classPtr = getRawPtr(expression.extensionReceiver!!)
@@ -690,7 +690,7 @@ internal class InteropLoweringPart1(val context: Context) : BaseInteropIrTransfo
                     builder.scope.scopeOwner.annotations.hasAnnotation(FqName("kotlin.native.internal.ExportForCppRuntime"))
 
             if (!useKotlinDispatch) {
-                val arguments = descriptor.valueParameters.map { expression.getValueArgument(it)!! }
+                val arguments = descriptor.valueParameters.map { expression.getValueArgument(it) }
                 assert(expression.dispatchReceiver == null || expression.extensionReceiver == null)
 
                 if (expression.superQualifier?.isObjCMetaClass() == true) {
@@ -774,7 +774,7 @@ internal class InteropLoweringPart1(val context: Context) : BaseInteropIrTransfo
     private fun IrBuilderWithScope.callAllocAndInit(
             classPtr: IrExpression,
             initMethodInfo: ObjCMethodInfo,
-            arguments: List<IrExpression>,
+            arguments: List<IrExpression?>,
             call: IrFunctionAccessExpression,
             initMethod: IrSimpleFunction
     ): IrExpression = irBlock(startOffset, endOffset) {
