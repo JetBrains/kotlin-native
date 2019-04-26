abea8d694ad95bed39f2e4e01c7a5a6c923b1f59
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2019-04-26 18:26:00 +0300

    [debugging][binding][python] speed up
    - reduce number of operaions on array processing and caching layout for objects with the same type info.

--- llvmDebugInfoC/src/scripts/konan_lldb.py
+++ llvmDebugInfoC/src/scripts/konan_lldb.py
@@ -316,7 +316,7 @@ class KonanProxyTypeProvider:
 
 def print_this_command(debugger, command, result, internal_dict):
     pthis = lldb.frame.FindVariable('<this>')
-    print(pthis)
+    log(pthis)
 
 def clear_cache_command(debugger, command, result, internal_dict):
     SYNTHETIC_OBJECT_LAYOUT_CACHE.clear()
