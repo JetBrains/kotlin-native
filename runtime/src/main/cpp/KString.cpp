b29c19cbe65714f9a783574f1890970f99e86c46
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2019-05-07 11:06:24 +0300

    String <-> CharArray, utf8 ByteArray common conversions
    #KT-24810, KT-29265

--- runtime/src/main/cpp/KString.cpp
+++ runtime/src/main/cpp/KString.cpp
@@ -35,7 +35,7 @@ typedef KStdStringInserter utf16to8(const KChar*,const KChar*, KStdStringInserte
 KStdStringInserter utf16toUtf8OrThrow(const KChar* start, const KChar* end, KStdStringInserter result) {
   TRY_CATCH(result = utf8::utf16to8(start, end, result),
             result = utf8::unchecked::utf16to8(start, end, result),
-            ThrowIllegalCharacterConversionException());
+            ThrowCharacterCodingException());
   return result;
 }
 
@@ -51,9 +51,6 @@ OBJ_GETTER(utf8ToUtf16Impl, const char* rawString, const char* end, uint32_t cha
 template<utf16to8 conversion>
 OBJ_GETTER(utf16ToUtf8Impl, KString thiz, KInt start, KInt size) {
   RuntimeAssert(thiz->type_info() == theStringTypeInfo, "Must use String");
-  if (start < 0 || size < 0 || (size > static_cast<KInt>(thiz->count_) - start)) {
-    ThrowArrayIndexOutOfBoundsException();
-  }
   const KChar* utf16 = CharArrayAddressOfElementAt(thiz, start);
   KStdString utf8;
   conversion(utf16, utf16 + size, back_inserter(utf8));
@@ -67,7 +64,7 @@ OBJ_GETTER(utf8ToUtf16OrThrow, const char* rawString, size_t rawStringLength) {
   uint32_t charCount;
   TRY_CATCH(charCount = utf8::utf16_length(rawString, end),
             charCount = utf8::unchecked::utf16_length(rawString, end),
-            ThrowIllegalCharacterConversionException());
+            ThrowCharacterCodingException());
   RETURN_RESULT_OF(utf8ToUtf16Impl<utf8::unchecked::utf8to16>, rawString, end, charCount);
 }
 
@@ -776,9 +773,6 @@ KInt Kotlin_String_getStringLength(KString thiz) {
 const char* byteArrayAsCString(KConstRef thiz, KInt start, KInt size) {
   const ArrayHeader* array = thiz->array();
   RuntimeAssert(array->type_info() == theByteArrayTypeInfo, "Must use a byte array");
-  if (start < 0 || size < 0 || size > array->count_ - start) {
-    ThrowArrayIndexOutOfBoundsException();
-  }
   return reinterpret_cast<const char*>(ByteArrayAddressOfElementAt(array, start));
 }
 
@@ -787,11 +781,7 @@ OBJ_GETTER(Kotlin_ByteArray_stringFromUtf8OrThrow, KConstRef thiz, KInt start, K
     RETURN_RESULT_OF0(TheEmptyString);
   }
   const char* rawString = byteArrayAsCString(thiz, start, size);
-  KInt realSize = 0;
-  while (rawString[realSize] != 0 && realSize < size) {
-     realSize++;
-  }
-  RETURN_RESULT_OF(utf8ToUtf16OrThrow, rawString, realSize);
+  RETURN_RESULT_OF(utf8ToUtf16OrThrow, rawString, size);
 }
 
 OBJ_GETTER(Kotlin_ByteArray_stringFromUtf8, KConstRef thiz, KInt start, KInt size) {
@@ -799,11 +789,7 @@ OBJ_GETTER(Kotlin_ByteArray_stringFromUtf8, KConstRef thiz, KInt start, KInt siz
     RETURN_RESULT_OF0(TheEmptyString);
   }
   const char* rawString = byteArrayAsCString(thiz, start, size);
-  KInt realSize = 0;
-  while (rawString[realSize] != 0 && realSize < size) {
-     realSize++;
-  }
-  RETURN_RESULT_OF(utf8ToUtf16, rawString, realSize);
+  RETURN_RESULT_OF(utf8ToUtf16, rawString, size);
 }
 
 OBJ_GETTER(Kotlin_String_toUtf8, KString thiz, KInt start, KInt size) {
@@ -817,9 +803,6 @@ OBJ_GETTER(Kotlin_String_toUtf8OrThrow, KString thiz, KInt start, KInt size) {
 OBJ_GETTER(Kotlin_String_fromCharArray, KConstRef thiz, KInt start, KInt size) {
   const ArrayHeader* array = thiz->array();
   RuntimeAssert(array->type_info() == theCharArrayTypeInfo, "Must use a char array");
-  if (start < 0 || size < 0 || size > array->count_ - start) {
-    ThrowArrayIndexOutOfBoundsException();
-  }
 
   if (size == 0) {
     RETURN_RESULT_OF0(TheEmptyString);
@@ -833,12 +816,12 @@ OBJ_GETTER(Kotlin_String_fromCharArray, KConstRef thiz, KInt start, KInt size) {
   RETURN_OBJ(result->obj());
 }
 
-OBJ_GETTER(Kotlin_String_toCharArray, KString string) {
+OBJ_GETTER(Kotlin_String_toCharArray, KString string, KInt start, KInt size) {
   ArrayHeader* result = AllocArrayInstance(
-    theCharArrayTypeInfo, string->count_, OBJ_RESULT)->array();
+    theCharArrayTypeInfo, size, OBJ_RESULT)->array();
   memcpy(CharArrayAddressOfElementAt(result, 0),
-         CharArrayAddressOfElementAt(string, 0),
-         string->count_ * sizeof(KChar));
+         CharArrayAddressOfElementAt(string, start),
+         size * sizeof(KChar));
   RETURN_OBJ(result->obj());
 }
 
