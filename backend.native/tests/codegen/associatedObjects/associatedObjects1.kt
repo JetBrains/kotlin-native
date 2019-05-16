ce866d3a9b62001d1546bb1f0f9c5ae2f9e5d44d
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2019-05-16 14:26:13 +0300

    fixup! Add initial experimental support for @AssociatedObjectKey

--- backend.native/tests/codegen/associatedObjects/associatedObjects1.kt
+++ backend.native/tests/codegen/associatedObjects/associatedObjects1.kt
@@ -10,7 +10,7 @@ import kotlin.reflect.*
 
 @Test
 @UseExperimental(ExperimentalAssociatedObjects::class)
-fun runTest() {
+fun testBasics1() {
     assertSame(Bar, Foo::class.findAssociatedObject<Associated1>())
     assertSame(Baz, Foo::class.findAssociatedObject<Associated2>())
     assertSame(null, Foo::class.findAssociatedObject<Associated3>())
@@ -39,3 +39,39 @@ class Foo
 
 object Bar
 object Baz
+
+@Test
+@UseExperimental(ExperimentalAssociatedObjects::class)
+fun testGlobalOptimizations1() {
+    val i1 = I1ImplHolder::class.findAssociatedObject<Associated1>()!! as I1
+    assertEquals(42, i1.foo())
+}
+
+private interface I1 {
+    fun foo(): Int
+}
+
+private object I1Impl : I1 {
+    override fun foo() = 42
+}
+
+@Associated1(I1Impl::class)
+private class I1ImplHolder
+
+@Test
+@UseExperimental(ExperimentalAssociatedObjects::class)
+fun testGlobalOptimizations2() {
+    val i2 = I2ImplHolder()::class.findAssociatedObject<Associated1>()!! as I2
+    assertEquals(17, i2.foo())
+}
+
+private interface I2 {
+    fun foo(): Int
+}
+
+private object I2Impl : I2 {
+    override fun foo() = 17
+}
+
+@Associated1(I2Impl::class)
+private class I2ImplHolder
--- runtime/src/main/kotlin/kotlin/reflect/AssociatedObjects.kt
+++ runtime/src/main/kotlin/kotlin/reflect/AssociatedObjects.kt
@@ -7,15 +7,40 @@ package kotlin.reflect
 
 import kotlin.native.internal.*
 
-@Retention(value = AnnotationRetention.BINARY)
+/**
+ * This annotation marks the associated objects API that is considered experimental and is not subject to the
+ * [general compatibility guarantees](https://kotlinlang.org/docs/reference/evolution/components-stability.html) given for the standard library:
+ * the behavior of such API may be changed or the API may be removed completely in any further release.
+ *
+ * > Beware using the annotated API especially if you're developing a library, since your library might become binary incompatible
+ * with the future versions of the standard library.
+ *
+ * Any usage of a declaration annotated with `@ExperimentalAssociatedObjects` must be accepted either by
+ * annotating that usage with the [UseExperimental] annotation, e.g. `@UseExperimental(ExperimentalAssociatedObjects::class)`,
+ * or by using the compiler argument `-Xuse-experimental=kotlin.reflect.ExperimentalAssociatedObjects`.
+ */
 @Experimental(level = Experimental.Level.ERROR)
-annotation class ExperimentalAssociatedObjects
+@Retention(value = AnnotationRetention.BINARY)
+public annotation class ExperimentalAssociatedObjects
 
+/**
+ * Makes the annotated annotation class an associated object key.
+ *
+ * An associated object key annotation should have single [KClass] parameter.
+ * When applied to a class with reference to a singleton as an argument, it binds
+ * the singleton instance to the class, making this binding discoverable at runtime using [findAssociatedObject].
+ */
 @ExperimentalAssociatedObjects
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.ANNOTATION_CLASS)
-annotation class AssociatedObjectKey
+public annotation class AssociatedObjectKey
 
+/**
+ * If [T] is an @[AssociatedObjectKey]-annotated annotation class and [this] class is annotatated with @[T] (`S::class`),
+ * returns the instance of singleton `S`.
+ *
+ * Otherwise returns `null`.
+ */
 @ExperimentalAssociatedObjects
-inline fun <reified T> KClass<*>.findAssociatedObject(): Any? =
+public inline fun <reified T> KClass<*>.findAssociatedObject(): Any? =
         this.findAssociatedObject(T::class)
