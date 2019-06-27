690748e6380513d26f7ec2c2da1339290934307e
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2019-06-27 05:08:58 +0300

    New zero-terminated utf8 to String conversion API

--- runtime/src/main/kotlin/kotlin/native/Text.kt
+++ runtime/src/main/kotlin/kotlin/native/Text.kt
@@ -9,34 +9,80 @@ package kotlin.native
 /**
  * Converts an UTF-8 array into a [String]. Replaces invalid input sequences with a default character.
  */
+@Deprecated("Use decodeToString or decodeZeroTerminatedToString instead")
 public fun ByteArray.stringFromUtf8() : String {
+    @Suppress("DEPRECATION")
     return this.stringFromUtf8(0, this.size)
 }
 
 /**
  * Converts an UTF-8 array into a [String]. Replaces invalid input sequences with a default character.
  */
+@Deprecated("Use decodeToString or decodeZeroTerminatedToString instead")
 public fun ByteArray.stringFromUtf8(start: Int = 0, size: Int = this.size) : String {
-    checkBoundsIndexes(start, start + size, this.size)
-    return stringFromUtf8Impl(start, realSize(this, size))
+    val endIndex = start + size
+    checkBoundsIndexes(start, endIndex, this.size)
+
+    val realSize = realEndIndex(this, start, endIndex) - start
+    return unsafeStringFromUtf8Impl(start, realSize)
 }
 
-@SymbolName("Kotlin_ByteArray_stringFromUtf8")
-internal external fun ByteArray.stringFromUtf8Impl(start: Int, size: Int) : String
+/**
+ * Decodes a string from the bytes in UTF-8 encoding in this array.
+ * Bytes following the first occurrence of `0` byte, if it occurs, are not decoded.
+ *
+ * Malformed byte sequences are replaced by the replacement char `\uFFFD`.
+ */
+@SinceKotlin("1.3")
+public fun ByteArray.decodeZeroTerminatedToString() : String {
+    return unsafeStringFromUtf8Impl(0, realEndIndex(this, 0, this.size))
+}
 
-private fun realSize(byteArray: ByteArray, size: Int): Int {
-    var realSize = 0
-    while (realSize < size && byteArray[realSize] != 0.toByte()) {
-        realSize++
+/**
+ * Decodes a string from the bytes in UTF-8 encoding in this array or its subrange.
+ * Bytes following the first occurrence of `0` byte, if it occurs, are not decoded.
+ *
+ * @param startIndex the beginning (inclusive) of the subrange to decode, 0 by default.
+ * @param endIndex the end (exclusive) of the subrange to decode, size of this array by default.
+ * @param throwOnInvalidSequence specifies whether to throw an exception on malformed byte sequence or replace it by the replacement char `\uFFFD`.
+ *
+ * @throws IndexOutOfBoundsException if [startIndex] is less than zero or [endIndex] is greater than the size of this array.
+ * @throws IllegalArgumentException if [startIndex] is greater than [endIndex].
+ * @throws CharacterCodingException if the byte array contains malformed UTF-8 byte sequence and [throwOnInvalidSequence] is true.
+ */
+@SinceKotlin("1.3")
+public fun ByteArray.decodeZeroTerminatedToString(
+        startIndex: Int = 0,
+        endIndex: Int = this.size,
+        throwOnInvalidSequence: Boolean = false
+) : String {
+    checkBoundsIndexes(startIndex, endIndex, this.size)
+
+    val realSize = realEndIndex(this, startIndex, endIndex) - startIndex
+    if (throwOnInvalidSequence) {
+        return unsafeStringFromUtf8OrThrowImpl(startIndex, realSize)
     }
-    return realSize
+    return unsafeStringFromUtf8Impl(startIndex, realSize)
+}
+
+@SymbolName("Kotlin_ByteArray_unsafeStringFromUtf8")
+internal external fun ByteArray.unsafeStringFromUtf8Impl(start: Int, size: Int) : String
+
+private fun realEndIndex(byteArray: ByteArray, startIndex: Int, endIndex: Int): Int {
+    var index = startIndex
+    while (index < endIndex && byteArray[index] != 0.toByte()) {
+        index++
+    }
+    return index
 }
 
 /**
  * Converts an UTF-8 array into a [String].
  * @throws [IllegalCharacterConversionException] if the input is invalid.
  */
+@Deprecated("Use decodeToString or decodeZeroTerminatedToString instead")
 public fun ByteArray.stringFromUtf8OrThrow() : String {
+    @Suppress("DEPRECATION")
     return this.stringFromUtf8OrThrow(0, this.size)
 }
 
@@ -44,17 +90,22 @@ public fun ByteArray.stringFromUtf8OrThrow() : String {
  * Converts an UTF-8 array into a [String].
  * @throws [IllegalCharacterConversionException] if the input is invalid.
  */
+@Deprecated("Use decodeToString or decodeZeroTerminatedToString instead")
 public fun ByteArray.stringFromUtf8OrThrow(start: Int = 0, size: Int = this.size) : String {
-    checkBoundsIndexes(start, start + size, this.size)
+    val endIndex = start + size
+    checkBoundsIndexes(start, endIndex, this.size)
+
+    val realSize = realEndIndex(this, start, endIndex) - start
     try {
-        return stringFromUtf8OrThrowImpl(start, realSize(this, size))
+        return unsafeStringFromUtf8OrThrowImpl(start, realSize)
     } catch (e: CharacterCodingException) {
+        @Suppress("DEPRECATION")
         throw IllegalCharacterConversionException()
     }
 }
 
-@SymbolName("Kotlin_ByteArray_stringFromUtf8OrThrow")
-internal external fun ByteArray.stringFromUtf8OrThrowImpl(start: Int, size: Int) : String
+@SymbolName("Kotlin_ByteArray_unsafeStringFromUtf8OrThrow")
+internal external fun ByteArray.unsafeStringFromUtf8OrThrowImpl(start: Int, size: Int) : String
 
 /**
  * Converts a [String] into an UTF-8 array. Replaces invalid input sequences with a default character.
@@ -71,11 +122,11 @@ public fun String.toUtf8() : ByteArray {
 @Deprecated("Use encodeToByteArray instead", ReplaceWith("encodeToByteArray(start, start + size)"))
 public fun String.toUtf8(start: Int = 0, size: Int = this.length) : ByteArray {
     checkBoundsIndexes(start, start + size, this.length)
-    return toUtf8Impl(start, size)
+    return unsafeToUtf8Impl(start, size)
 }
 
-@SymbolName("Kotlin_String_toUtf8")
-internal external fun String.toUtf8Impl(start: Int, size: Int) : ByteArray
+@SymbolName("Kotlin_String_unsafeToUtf8")
+internal external fun String.unsafeToUtf8Impl(start: Int, size: Int) : ByteArray
 
 /**
  * Converts a [String] into an UTF-8 array.
@@ -95,8 +146,9 @@ public fun String.toUtf8OrThrow() : ByteArray {
 public fun String.toUtf8OrThrow(start: Int = 0, size: Int = this.length) : ByteArray {
     checkBoundsIndexes(start, start + size, this.length)
     try {
-        return toUtf8OrThrowImpl(start, size)
+        return unsafeToUtf8OrThrowImpl(start, size)
     } catch (e: CharacterCodingException) {
+        @Suppress("DEPRECATION")
         throw IllegalCharacterConversionException()
     }
 }
@@ -110,11 +162,11 @@ internal fun checkBoundsIndexes(startIndex: Int, endIndex: Int, size: Int) {
     }
 }
 
-@SymbolName("Kotlin_String_toUtf8OrThrow")
-internal external fun String.toUtf8OrThrowImpl(start: Int, size: Int) : ByteArray
+@SymbolName("Kotlin_String_unsafeToUtf8OrThrow")
+internal external fun String.unsafeToUtf8OrThrowImpl(start: Int, size: Int) : ByteArray
 
-@SymbolName("Kotlin_String_fromCharArray")
-internal external fun fromCharArray(array: CharArray, start: Int, size: Int) : String
+@SymbolName("Kotlin_String_unsafeFromCharArray")
+internal external fun unsafeFromCharArray(array: CharArray, start: Int, size: Int) : String
 
 @SymbolName("Kotlin_StringBuilder_insertString")
 internal external fun insertString(array: CharArray, start: Int, value: String): Int
