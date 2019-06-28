dd845f411a92a6549663cf374b9016a197991ddc
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2019-06-28 15:32:19 +0300

    Very preliminary relaxed mode draft.

--- backend.native/tests/runtime/workers/worker10.kt
+++ backend.native/tests/runtime/workers/worker10.kt
@@ -45,7 +45,7 @@ val topSharedData = Data(43)
         false
     }
     }).consume {
-        result -> assertEquals(false, result)
+        result -> assertEquals(Platform.memoryModel == MemoryModel.RELAXED, result)
     }
 
     worker.execute(TransferMode.SAFE, { -> }, {
@@ -65,7 +65,7 @@ val topSharedData = Data(43)
             false
         }
     }).consume {
-        result -> assertEquals(false, result)
+        result -> assertEquals(Platform.memoryModel == MemoryModel.RELAXED, result)
     }
 
     worker.execute(TransferMode.SAFE, { -> }, {
