a66607c5f0e5b535d08e098a949ad76e74ca80d9
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2019-09-06 12:36:41 +0300

    Small fix for points in module names

--- backend.native/cli.bc/src/org/jetbrains/kotlin/cli/bc/K2Native.kt
+++ backend.native/cli.bc/src/org/jetbrains/kotlin/cli/bc/K2Native.kt
@@ -119,12 +119,7 @@ class K2Native : CLICompiler<K2NativeCompilerArguments>() {
                         arguments.libraries.toNonNullList())
                 put(LINKER_ARGS, arguments.linkerArguments.toNonNullList() +
                         arguments.singleLinkerArguments.toNonNullList())
-                arguments.moduleName ?. let {
-                    if (it.contains('.')) {
-                        report(WARNING, "Module name with points may cause problems during using in ObjectiveC interop.")
-                    }
-                    put(MODULE_NAME, it)
-                }
+                arguments.moduleName ?. let{ put(MODULE_NAME, it) }
                 arguments.target ?.let{ put(TARGET, it) }
 
                 put(INCLUDED_BINARY_FILES,
