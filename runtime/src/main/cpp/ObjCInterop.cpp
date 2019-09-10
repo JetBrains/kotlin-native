269f8df11ec6df0ae277a3f0bad0539cccb4e294
Vladimir Ivanov <vladimir.d.ivanov@jetbrains.com>
2019-09-10 19:38:57 +0300

    [KT-32929] feedback: use annotation

--- runtime/src/main/cpp/ObjCInterop.cpp
+++ runtime/src/main/cpp/ObjCInterop.cpp
@@ -201,6 +201,10 @@ void Kotlin_objc_autoreleasePoolPop(void* ptr) {
   objc_autoreleasePoolPop(ptr);
 }
 
+id Kotlin_objc_allocWithZone(Class clazz) {
+  return objc_allocWithZone(clazz);
+}
+
 id Kotlin_objc_retain(id ptr) {
   return objc_retain(ptr);
 }
