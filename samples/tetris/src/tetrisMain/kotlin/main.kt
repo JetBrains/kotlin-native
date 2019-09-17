ded8531daea4491f234b08b854e2ea7df705153e
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2019-09-17 15:10:01 +0300

    Small fixes

--- samples/tetris/src/tetrisMain/kotlin/main.kt
+++ samples/tetris/src/tetrisMain/kotlin/main.kt
@@ -10,8 +10,8 @@ import kotlinx.cli.*
 fun main(args: Array<String>) {
     val argParser = ArgParser("tetris", useDefaultHelpShortName = false)
     val level by argParser.option(ArgType.Int, shortName = "l", description = "Game level").default(Config.startLevel)
-    val width by argParser.option(ArgType.Int, shortName = "w", description = "Width of game field").default(Config.width)
-    val height by argParser.option(ArgType.Int, shortName = "h", description = "Height of game field").default(Config.height)
+    val width by argParser.option(ArgType.Int, shortName = "w", description = "Width of the game field").default(Config.width)
+    val height by argParser.option(ArgType.Int, shortName = "h", description = "Height of the game field").default(Config.height)
     argParser.parse(args)
     val visualizer = SDL_Visualizer(width, height)
     val game = Game(width, height, visualizer, visualizer)
