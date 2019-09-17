66f58ef11e6bcf3441bb8976130c96ff1c5e37f4
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2019-09-17 15:28:23 +0300

    Format

--- samples/videoplayer/src/videoPlayerMain/kotlin/VideoPlayer.kt
+++ samples/videoplayer/src/videoPlayerMain/kotlin/VideoPlayer.kt
@@ -158,8 +158,9 @@ class VideoPlayer(val requestedSize: Dimensions?) : DisposableContainer() {
 
 fun main(args: Array<String>) {
     val argParser = ArgParser("videoplayer")
-    val mode by argParser.option(ArgType.Choice(listOf("video", "audio", "both")),
-            shortName = "m", description = "Play mode").default("both")
+    val mode by argParser.option(
+            ArgType.Choice(listOf("video", "audio", "both")), shortName = "m", description = "Play mode")
+            .default("both")
     val size by argParser.option(ArgType.Int, shortName = "s", description = "Required size of videoplayer window")
             .delimiter(",")
     val fileName by argParser.argument(ArgType.String, description = "File to play")
