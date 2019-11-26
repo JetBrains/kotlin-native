a3378f6f9b118e08040050754c87c8e66b4b55dc
Nikolay Igotti <igotti@gmail.com>
2019-11-26 11:13:43 +0300

    Shared cyclic garbage detector.

--- runtime/src/main/kotlin/kotlin/native/internal/GC.kt
+++ runtime/src/main/kotlin/kotlin/native/internal/GC.kt
@@ -78,6 +78,19 @@ object GC {
         get() = getTuneThreshold()
         set(value) = setTuneThreshold(value)
 
+    /**
+     * Detect cyclic references going via atomic references and return list of cycle-inducing objects
+     * or `null` if the leak detector is not available.
+     */
+    @SymbolName("Kotlin_native_internal_GC_detectCycles")
+    external fun detectCycles(): Array<Any>?
+
+    /**
+     * Find a cycle starting from the given object, `null` if there are no cycles.
+     */
+    @SymbolName("Kotlin_native_internal_GC_findCycle")
+    external fun findCycle(root: Any): Array<Any>?
+
     @SymbolName("Kotlin_native_internal_GC_getThreshold")
     private external fun getThreshold(): Int
 
