1f5e3f792bc966ce4251282fb9a3d4dd82325fa1
Alexander Kuznetsov <agascogen@gmail.com>
2019-12-24 22:09:58 +0300

    [backend][specialization] add more samples for `receiver.kt`
    Specialization never helps in these cases, since there are no specialization for classes yet, and equality comparison in `GenericBox#eqls__` requires boxing of specialized value `other` (it is passed to generated function as value type).

--- boxing/receiver.kt
+++ boxing/receiver.kt
@@ -1,20 +1,39 @@
 package receiver
 
-class Box(val x: Int?) {
-    fun default__() = if (x is Int) x - 1 else 0
+class Box(val x: Int) {
+    fun default__() = x - 1
+}
+
+class GenericBox<T>(val value: T) {
+    fun default__() = if (value is Int) value - 1 else 0
 }
 
 fun <T> Box.doDefault__(): Int {
     return this.default__()
 }
 
-fun Box.doDefault___Int(): Int {
+fun <T> GenericBox<T>.doDefault__(): Int {
     return this.default__()
 }
 
+fun <T> doDefault__(box: GenericBox<T>) : Int {
+    return box.default__()
+}
+
+fun <T> GenericBox<T>.doDefault__(other: GenericBox<T>) = default__() + other.default__()
+
+fun <T> T.eqls__(other: T) = this == other
+
+fun <T> GenericBox<T>.eqls__(other: T) = this == other
+
 fun runCount(): Int {
     for (i in 1..10) {
-        Box(i).doDefault__<Int>()
+        Box(i).doDefault__<Int>()                                    // 0 boxings
+        GenericBox(i).doDefault__()                                  // 10 boxings
+        doDefault__(GenericBox(i + 1))                               // 10 boxings
+        GenericBox(i - 1).doDefault__(GenericBox(i + 1))             // 20 boxings
+        1.eqls__(2)                                                  // 0 boxings
+        GenericBox(1).eqls__(2)                                      // 20 boxings
     }
     return 0
 }
