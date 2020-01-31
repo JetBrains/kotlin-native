c71d9c5f12b9bbc517ab246b5bcc5d91ff84c1e6
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-01-31 20:50:42 +0700

    Add missing copyrights

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ir/interop/cstruct/CStructVarClassGenerator.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ir/interop/cstruct/CStructVarClassGenerator.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the LICENSE file.
+ */
 package org.jetbrains.kotlin.backend.konan.ir.interop.cstruct
 
 import org.jetbrains.kotlin.backend.konan.InteropBuiltIns
