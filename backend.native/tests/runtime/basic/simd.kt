d4f6111aede1fa25ce34a29c59d1354577187e08
Nikolay Igotti <nikolay.igotti@jetbrains.com>
2020-02-03 13:37:39 +0300

    Add more vector types.

--- backend.native/tests/runtime/basic/simd.kt
+++ backend.native/tests/runtime/basic/simd.kt
@@ -7,12 +7,12 @@ package runtime.basic.simd
 
 import kotlin.test.*
 
-
-@Test fun runTest() {
+@Test fun runTest1() {
     testBoxingSimple()
     testBoxing()
     testSetGet()
-    testString()
+    testString1()
+    testString2()
     testOOB()
     testEquals()
     testHash()
@@ -31,34 +31,39 @@ class UnalignedC(t: Vector128) {
 
 fun testBoxingSimple() {
     val v = vectorOf(1f, 3.162f, 10f, 31f)
-    val box: Box<Vector128> = Box<Vector128>(v)
+    val box: Box<VectorFloat4> = Box<VectorFloat4>(v)
     assertEquals(v, box.value, "testBoxingSimple FAILED")
 }
 
 fun testBoxing() {
-    var u = UnalignedC(vectorOf(0, 1, 2, 3))
+    var u = UnalignedC(genericVectorOf(0, 1, 2, 3))
     assertEquals(3, u.value.getIntAt(3))
-    u.value = vectorOf(0f, 1f, 2f, 3f)
-    assertEquals(vectorOf(0f, 1f, 2f, 3f), u.value, "testBoxing FAILED")
+    u.value = genericVectorOf(0f, 1f, 2f, 3f)
+    assertEquals(genericVectorOf(0f, 1f, 2f, 3f), u.value, "testBoxing FAILED")
 }
 
 fun testSetGet() {
-    var v4any = vectorOf(0, 1, 2, 3)
+    var v4any = genericVectorOf(0, 1, 2, 3)
     (0 until 4).forEach { assertEquals(it, v4any.getIntAt(it), "testSetGet FAILED for <4 x i32>") }
 
     // type punning: set the variable to another runtime type
     val a = arrayOf(1f, 3.162f, 10f, 31f)
-    v4any = vectorOf(a[0], a[1], a[2], a[3])
+    v4any = genericVectorOf(a[0], a[1], a[2], a[3])
     (0 until 4).forEach { assertEquals(a[it], v4any.getFloatAt(it), "testSetGet FAILED for <4 x float>") }
 }
 
-fun testString() {
+fun testString1() {
     val v4i = vectorOf(100, 1024, Int.MAX_VALUE, Int.MIN_VALUE)
     assertEquals("(0x64, 0x400, 0x7fffffff, 0x80000000)", v4i.toString(), "testString FAILED")
 }
 
+fun testString2() {
+    val v4f = vectorOf(1.0f, 2.0f, 0.5f, -0.5f)
+    assertEquals("(1.0, 2.0, 0.5, -0.5)", v4f.toString(), "testString FAILED")
+}
+
 fun testOOB() {
-    val f = vectorOf(1f, 3.162f, 10f, 31f)
+    val f = genericVectorOf(1f, 3.162f, 10f, 31f)
 
     println("f.getByteAt(15) is OK ${f.getByteAt(15)}")
 
@@ -76,8 +81,8 @@ fun testOOB() {
 }
 
 fun testEquals() {
-    var v1 = vectorOf(-1f, 0f, 0f, -7f)
-    var v2 = vectorOf(1f, 4f, 3f, 7f)
+    var v1 = genericVectorOf(-1f, 0f, 0f, -7f)
+    var v2 = genericVectorOf(1f, 4f, 3f, 7f)
     assertEquals(false, v1 == v2)
     assertEquals(false, v1.equals(v2))
     assertEquals(false, v1.equals(Any()))
@@ -85,6 +90,12 @@ fun testEquals() {
 
     v1 = v2
     assertEquals(true, v1 == v2)
+
+    var v3 = vectorOf(1, 2, 3)
+    var v4 = vectorOf(1, 2, 2)
+    assertTrue(v3 != v4)
+    v4 = vectorOf(1, 2, 3)
+    assertEquals(v3, v4)
 }
 
 fun testHash() {
@@ -93,12 +104,12 @@ fun testHash() {
     assertEquals(false, h1 == h2)
 
     val i = 654687
-    assertEquals(true, vectorOf(0, 0, i, 0).hashCode() == i.hashCode())  // exploit little endianness
-    assertEquals(true, vectorOf(1, 0, 0, 0).hashCode() == vectorOf(0, 0, 31, 0).hashCode())
+    assertEquals(true, genericVectorOf(0, 0, i, 0).hashCode() == i.hashCode())  // exploit little endianness
+    assertEquals(true, genericVectorOf(1, 0, 0, 0).hashCode() == genericVectorOf(0, 0, 31, 0).hashCode())
 }
 
-private fun funDefaultValue(v: Vector128 = vectorOf(1.0f, 2.0f, 3.0f, 4.0f)) = v
+private fun funDefaultValue(v: Vector128 = genericVectorOf(1.0f, 2.0f, 3.0f, 4.0f)) = v
 
 fun testDefaultValue() {
-    assertEquals(vectorOf(1.0f, 2.0f, 3.0f, 4.0f), funDefaultValue())
+    assertEquals(genericVectorOf(1.0f, 2.0f, 3.0f, 4.0f), funDefaultValue())
 }
--- runtime/src/main/kotlin/kotlin/native/simd.kt
+++ runtime/src/main/kotlin/kotlin/native/simd.kt
@@ -7,31 +7,30 @@ package kotlin.native
 import kotlin.native.internal.TypedIntrinsic
 import kotlin.native.internal.IntrinsicType
 
-
 public final class Vector128 private constructor() {
     @TypedIntrinsic(IntrinsicType.EXTRACT_ELEMENT)
-    external fun getByteAt(index: Int): Byte
+    external public fun getByteAt(index: Int): Byte
 
     @TypedIntrinsic(IntrinsicType.EXTRACT_ELEMENT)
-    external fun getIntAt(index: Int): Int
+    external public fun getIntAt(index: Int): Int
 
     @TypedIntrinsic(IntrinsicType.EXTRACT_ELEMENT)
-    external fun getLongAt(index: Int): Long
+    external public fun getLongAt(index: Int): Long
 
     @TypedIntrinsic(IntrinsicType.EXTRACT_ELEMENT)
-    external fun getFloatAt(index: Int): Float
+    external public fun getFloatAt(index: Int): Float
 
     @TypedIntrinsic(IntrinsicType.EXTRACT_ELEMENT)
-    external fun getDoubleAt(index: Int): Double
+    external public fun getDoubleAt(index: Int): Double
 
     @TypedIntrinsic(IntrinsicType.EXTRACT_ELEMENT)
-    external fun getUByteAt(index: Int): UByte
+    external public fun getUByteAt(index: Int): UByte
 
     @TypedIntrinsic(IntrinsicType.EXTRACT_ELEMENT)
-    external fun getUIntAt(index: Int): UInt
+    external public fun getUIntAt(index: Int): UInt
 
     @TypedIntrinsic(IntrinsicType.EXTRACT_ELEMENT)
-    external fun getULongAt(index: Int): ULong
+    external public fun getULongAt(index: Int): ULong
 
     public override fun toString() =
             "(0x${getUIntAt(0).toString(16)}, 0x${getUIntAt(1).toString(16)}, 0x${getUIntAt(2).toString(16)}, 0x${getUIntAt(3).toString(16)})"
@@ -43,15 +42,71 @@ public final class Vector128 private constructor() {
     public override fun equals(other: Any?): Boolean =
             other is Vector128 && this.equals(other)
 
-    override fun hashCode(): Int {
+    override public fun hashCode(): Int {
         val x0 = getLongAt(0)
         val x1 = getLongAt(1)
         return 31 * (x0 xor (x0 shr 32)).toInt() + (x1 xor (x1 shr 32)).toInt()
     }
 }
 
+@SinceKotlin("1.4")
+public inline class VectorFloat3(internal val data: Vector128) {
+    public operator fun get(index: Int): Float = data.getFloatAt(index)
+
+    public fun getIntAt(index: Int): Int = data.getIntAt(index)
+    public fun getUIntAt(index: Int): UInt = data.getUIntAt(index)
+
+    public override fun toString(): String =
+            "(${this[0]}, ${this[1]}, ${this[2]})"
+}
+
+@SinceKotlin("1.4")
+public inline class VectorFloat4(internal val data: Vector128) {
+    public operator fun get(index: Int): Float = data.getFloatAt(index)
+
+    public fun getIntAt(index: Int): Int = data.getIntAt(index)
+    public fun getUIntAt(index: Int): UInt = data.getUIntAt(index)
+
+    public override fun toString(): String =
+            "(${this[0]}, ${this[1]}, ${this[2]}, ${this[3]})"
+}
+
+@SinceKotlin("1.4")
+public inline class VectorInt3(internal val data: Vector128){
+    public operator fun get(index: Int): Int = data.getIntAt(index)
+
+    public fun getUIntAt(index: Int): UInt = data.getUIntAt(index)
+
+    public override fun toString(): String =
+            "(0x${getUIntAt(0).toString(16)}, 0x${getUIntAt(1).toString(16)}, 0x${getUIntAt(2).toString(16)}})"
+}
+
+@SinceKotlin("1.4")
+public inline class VectorInt4(internal val data: Vector128) {
+    public operator fun get(index: Int): Int = data.getIntAt(index)
+
+    public fun getUIntAt(index: Int): UInt = data.getUIntAt(index)
+
+    public override fun toString() =
+            "(0x${getUIntAt(0).toString(16)}, 0x${getUIntAt(1).toString(16)}, 0x${getUIntAt(2).toString(16)}, 0x${getUIntAt(3).toString(16)})"
+}
+
 @SymbolName("Kotlin_Vector4f_of")
-external fun vectorOf(f0: Float, f1: Float, f2: Float, f3: Float): Vector128
+public external fun genericVectorOf(f0: Float, f1: Float, f2: Float, f3: Float): Vector128
+
+@SinceKotlin("1.4")
+public fun vectorOf(f0: Float, f1: Float, f2: Float): VectorFloat3
+        = VectorFloat3(genericVectorOf(f0, f1, f2, 0.0f))
+@SinceKotlin("1.4")
+public fun vectorOf(f0: Float, f1: Float, f2: Float, f3: Float): VectorFloat4
+        = VectorFloat4(genericVectorOf(f0, f1, f2, f3))
 
 @SymbolName("Kotlin_Vector4i32_of")
-external fun vectorOf(f0: Int, f1: Int, f2: Int, f3: Int): Vector128
+public external fun genericVectorOf(i0: Int, i1: Int, i2: Int, i3: Int): Vector128
+
+@SinceKotlin("1.4")
+public fun vectorOf(i0: Int, i1: Int, i2: Int): VectorInt3
+        = VectorInt3(genericVectorOf(i0, i1, i2, 0))
+@SinceKotlin("1.4")
+public fun vectorOf(i0: Int, i1: Int, i2: Int, i3: Int): VectorInt4
+        = VectorInt4(genericVectorOf(i0, i1, i2, i3))
