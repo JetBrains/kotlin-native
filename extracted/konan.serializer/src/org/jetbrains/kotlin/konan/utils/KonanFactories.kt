56a57237b6588dd70b5b25c15e2f67847f72b936
Roman Artemev <roman.artemev@jetbrains.com>
2020-02-17 17:58:53 +0300

    [Metadata] Improve Forward-declaration related hack

--- extracted/konan.serializer/src/org/jetbrains/kotlin/konan/utils/KonanFactories.kt
+++ extracted/konan.serializer/src/org/jetbrains/kotlin/konan/utils/KonanFactories.kt
@@ -2,11 +2,12 @@ package org.jetbrains.kotlin.konan.utils
 
 import org.jetbrains.kotlin.builtins.konan.KonanBuiltIns
 import org.jetbrains.kotlin.konan.util.KlibMetadataFactories
-import org.jetbrains.kotlin.serialization.konan.NullFlexibleTypeDeserializer
+import org.jetbrains.kotlin.library.metadata.NativeTypeTransformer
+import org.jetbrains.kotlin.library.metadata.NullFlexibleTypeDeserializer
 import org.jetbrains.kotlin.storage.StorageManager
 
 fun createKonanBuiltIns(storageManager: StorageManager) = KonanBuiltIns(storageManager)
 /**
  * The default Kotlin/Native factories.
  */
-object KonanFactories : KlibMetadataFactories(::createKonanBuiltIns, NullFlexibleTypeDeserializer)
+object KonanFactories : KlibMetadataFactories(::createKonanBuiltIns, NullFlexibleTypeDeserializer, NativeTypeTransformer())
