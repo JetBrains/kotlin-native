cb3b00b31af06d19fdd0033c0c30ec1416c723a8
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-03-04 17:13:02 +0700

    fixup! [Interop][Serialization] Skip files with C enums and structs

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/KonanIrModuleSerializer.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/serialization/KonanIrModuleSerializer.kt
@@ -21,6 +21,10 @@ class KonanIrModuleSerializer(
 
     // We skip files with IR for C structs and enums because they should be
     // generated anew.
+    //
+    // See [IrProviderForCEnumAndCStructStubs.kt#L31] on why we generate IR.
+    // We may switch from IR generation to LazyIR later (at least for structs; enums are tricky)
+    // without changing kotlin libraries that depend on interop libraries.
     override fun backendSpecificFileFilter(file: IrFile): Boolean =
             file.fileEntry.name != IrProviderForCEnumAndCStructStubs.cTypeDefinitionsFileName
 
