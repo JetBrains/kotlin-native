c916dd80537483343021c626886947ab5d60642e
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2020-03-31 13:44:14 +0200

    [runtime][source info][mac] more precision source info fetching

--- runtime/src/debug/cpp/SourceInfo.cpp
+++ runtime/src/debug/cpp/SourceInfo.cpp
@@ -17,7 +17,7 @@
 #include "SourceInfo.h"
 
 #ifdef KONAN_CORE_SYMBOLICATION
-
+#include <KAssert.h>
 #include <dlfcn.h>
 #include <limits.h>
 #include <stdint.h>
@@ -64,9 +64,18 @@ uint32_t (*CSSourceInfoGetLineNumber)(CSSourceInfoRef info);
 
 uint32_t (*CSSourceInfoGetColumn)(CSSourceInfoRef info);
 
+void* (*CSSymbolOwnerGetBaseAddress)(CSSymbolOwnerRef info);
 
 bool (*CSIsNull)(CSTypeRef);
+void (*CSShow)(CSTypeRef);
+CSSymbolRef (*CSSourceInfoGetSymbol)(CSSourceInfoRef info);
 
+typedef int (^CSSourceInfoIterator)(CSSourceInfoRef);
+int (*CSSymbolForeachSourceInfo)(CSSymbolRef, CSSourceInfoIterator);
+
+CSRange (*CSSourceInfoGetRange)(CSSourceInfoRef);
+
+CSSymbolRef (*CSSymbolOwnerGetSymbolWithAddress)(CSSymbolOwnerRef, unsigned long long);
 CSSymbolicatorRef symbolicator;
 
 bool TryInitializeCoreSymbolication() {
@@ -81,8 +90,13 @@ bool TryInitializeCoreSymbolication() {
   KONAN_CS_LOOKUP(CSSourceInfoGetPath)
   KONAN_CS_LOOKUP(CSSourceInfoGetLineNumber)
   KONAN_CS_LOOKUP(CSSourceInfoGetColumn)
+  KONAN_CS_LOOKUP(CSSymbolOwnerGetBaseAddress)
   KONAN_CS_LOOKUP(CSIsNull)
-
+  KONAN_CS_LOOKUP(CSShow)
+  KONAN_CS_LOOKUP(CSSourceInfoGetSymbol)
+  KONAN_CS_LOOKUP(CSSymbolForeachSourceInfo)
+  KONAN_CS_LOOKUP(CSSymbolOwnerGetSymbolWithAddress)
+  KONAN_CS_LOOKUP(CSSourceInfoGetRange)
 #undef KONAN_CS_LOOKUP
 
   symbolicator = CSSymbolicatorCreateWithPid(getpid());
@@ -92,7 +106,7 @@ bool TryInitializeCoreSymbolication() {
 } // namespace
 
 extern "C" struct SourceInfo Kotlin_getSourceInfo(void* addr) {
-  SourceInfo result = { .fileName = nullptr, .lineNumber = -1, .column = -1 };
+  __block SourceInfo result = { .fileName = nullptr, .lineNumber = -1, .column = -1 };
 
   static bool csIsAvailable = TryInitializeCoreSymbolication();
 
@@ -100,20 +114,30 @@ extern "C" struct SourceInfo Kotlin_getSourceInfo(void* addr) {
     unsigned long long address = static_cast<unsigned long long>((uintptr_t)addr);
 
     CSSymbolOwnerRef symbolOwner = CSSymbolicatorGetSymbolOwnerWithAddressAtTime(symbolicator, address, kCSNow);
-    CSSourceInfoRef sourceInfo = CSSymbolOwnerGetSourceInfoWithAddress(symbolOwner, address);
-    if (!CSIsNull(sourceInfo)) {
-      const char* fileName = CSSourceInfoGetPath(sourceInfo);
-      if (fileName != nullptr) {
-        result.fileName = fileName;
-        uint32_t lineNumber = CSSourceInfoGetLineNumber(sourceInfo);
-        if (lineNumber != 0) {
-          result.lineNumber = lineNumber;
-          result.column = CSSourceInfoGetColumn(sourceInfo);
-        }
-      }
-    }
-  }
+    if (CSIsNull(symbolOwner))
+      return result;
+    CSSymbolRef symbol = CSSymbolOwnerGetSymbolWithAddress(symbolOwner, address);
+    if (CSIsNull(symbol))
+      return result;
 
+    CSSymbolForeachSourceInfo(symbol,
+      ^(CSSourceInfoRef ref) {
+          uint32_t lineNumber = CSSourceInfoGetLineNumber(ref);
+          CSRange range = CSSourceInfoGetRange(ref);
+          if (lineNumber != 0
+              && address >= range.location
+              && address < range.location + range.length) {
+            RuntimeAssert(result.fileName == nullptr, "shouldn't be more than one value per address");
+            const char* fileName = CSSourceInfoGetPath(ref);
+            if (fileName != nullptr) {
+              result.fileName = fileName;
+              result.lineNumber = lineNumber;
+              result.column = CSSourceInfoGetColumn(ref);
+            }
+       }
+       return 1;
+   });
+  }
   return result;
 }
 
