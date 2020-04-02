4d620481b3d7c7ad972471fabacdb1df25f6edae
Alexander Kuznetsov <agascogen@gmail.com>
2020-04-02 16:27:03 +0300

    [backend][benchmarks] add forgotten annotations

--- performance/ring/src/main/kotlin/org/jetbrains/ring/Data.kt
+++ performance/ring/src/main/kotlin/org/jetbrains/ring/Data.kt
@@ -16,6 +16,38 @@
 
 package org.jetbrains.ring
 
+import kotlin.reflect.KClass
+
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.TYPE_PARAMETER)
+internal annotation class Specialized(
+        val forTypes: Array<KClass<*>> = [
+            Boolean::class,
+            Byte::class,
+            Char::class,
+            Double::class,
+            Float::class,
+            Int::class,
+            Long::class,
+            Short::class
+        ]
+)
+
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.CLASS)
+internal annotation class SpecializedClass(
+        val forTypes: Array<KClass<*>> = [
+            Boolean::class,
+            Byte::class,
+            Char::class,
+            Double::class,
+            Float::class,
+            Int::class,
+            Long::class,
+            Short::class
+        ]
+)
+
 fun classValues(size: Int): Iterable<Value> {
     return intValues(size).map { Value(it) }
 }
