d84fddacb27d4b8bc7947a76d29df4ae7dbfc447
Vladimir Ivanov <vladimir.d.ivanov@jetbrains.com>
2020-04-11 13:39:33 +0300

    (~) c++17 style Scoped guard

--- runtime/src/main/cpp/Utils.h
+++ runtime/src/main/cpp/Utils.h
@@ -14,6 +14,9 @@
  * limitations under the License.
  */
 
+#ifndef RUNTIME_UTILS_H
+#define RUNTIME_UTILS_H
+
 #include <cstdint>
 #include "KAssert.h"
 
@@ -53,3 +56,43 @@ class LockGuard {
   LockGuard(const LockGuard&) = delete;
   LockGuard& operator=(const LockGuard&) = delete;
 };
+
+
+namespace kstd { // TODO: suggest better name
+
+// Should I test for C++17 (more general) instead? #if __cplusplus >= 201703L
+#ifdef __cpp_deduction_guides
+
+/**
+ * Usage:
+ *   @code{.cpp}
+ *   auto guard = Scoped {
+ *       [&]() { lock(x); },    // lock now (constructor)
+ *       [&]() { unlock(x); }   // unlock at block exit (destructor)
+ *   };
+ *   // Another example, at exit only, no ctor, alternative form of declaring
+ *   Scoped atBlockExit { []() {
+ *       log_trace << "Nested scoped guard: this should happen right before unlock!";
+ *   }};
+ *   @endcode
+ */
+template <class Exit, class Enter = Exit > class Scoped {
+	const Exit atExit_;
+	Scoped(const Scoped&) = delete;
+	Scoped(Scoped&&) = delete;
+	Scoped& operator=(const Scoped& other) = delete;
+	Scoped& operator=(Scoped&&) = delete;
+public:
+	explicit Scoped(const Exit& teardown) : atExit_(teardown) {}
+	Scoped(const Enter& init, const Exit& teardown) : Scoped(teardown) { init(); }
+	~Scoped() { atExit_(); }
+};
+
+template <class F2> Scoped(F2) -> Scoped<F2, F2>;  //!<  At exit only
+template <class F1, class F2> Scoped(F1, F2) -> Scoped<F2, F1>;  //!< Both ctor and dtor
+
+#endif // #ifdef __cpp_deduction_guides
+
+}
+
+#endif // RUNTIME_UTILS_H
