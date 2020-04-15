8135e1e56bf5f9067275c313d848d47e9a4b1b35
Alexander Kuznetsov <agascogen@gmail.com>
2020-04-16 00:33:16 +0300

    [backend][cfg] add some tests on building cfg for loops
    - empty loop
    - loop with inner `if`
    - loop with inner `if` and `return`

--- backend.native/boxing.perf/src/macosMain/kotlin/samples/cfg/loop.kt
+++ backend.native/boxing.perf/src/macosMain/kotlin/samples/cfg/loop.kt
@@ -28,8 +28,43 @@ fun simpleWhileLoop(): Int {
     return x
 }
 
+fun whileLoopWithIf(): Int {
+    var x = 1
+    var i = 0
+    while (i < 10) {
+        if (i % 2 == 0) {
+            x += i
+        } else {
+            x -= i
+        }
+        i++
+    }
+    return x
+}
+
+fun whileLoopWithIfAndReturn(): Int {
+    var x = 1
+    var i = 0
+    while (i < 10) {
+        if (i % 2 == 0) {
+            x += i
+        } else {
+            return x
+        }
+        i++
+    }
+    return x
+}
+
+fun emptyLoop() {
+    for (i in 1..10) {}
+}
+
 fun cfgLoop() {
     simpleForLoop()
     simpleDoWhileLoop()
     simpleWhileLoop()
+    whileLoopWithIf()
+    whileLoopWithIfAndReturn()
+    emptyLoop()
 }
