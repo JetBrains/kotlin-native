0db79dd1b771262d978c6f3f74b46200877218dc
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2020-05-01 02:22:05 +0300

    Decommonize collection builder implementations

--- backend.native/tests/runtime/collections/hash_map0.kt
+++ backend.native/tests/runtime/collections/hash_map0.kt
@@ -155,7 +155,6 @@ fun testEquals() {
     assertFalse(m == mapOf("a" to "1", "b" to "2", "c" to "5"))
     assertFalse(m == mapOf("a" to "1", "b" to "2"))
     assertEquals(m.keys, expected.keys)
-    assertEquals(m.values, expected.values)
     assertEquals(m.entries, expected.entries)
 }
 
@@ -165,7 +164,6 @@ fun testHashCode() {
     assertEquals(expected.hashCode(), m.hashCode())
     assertEquals(expected.entries.hashCode(), m.entries.hashCode())
     assertEquals(expected.keys.hashCode(), m.keys.hashCode())
-    assertEquals(listOf("1", "2", "3").hashCode(), m.values.hashCode())
 }
 
 fun testToString() {
@@ -184,9 +182,9 @@ fun testPutEntry() {
     assertTrue(m.entries.contains(e))
     assertTrue(m.entries.remove(e))
     assertTrue(mapOf("b" to "2", "c" to "3") == m)
-    assertTrue(m.entries.add(e))
+    assertEquals(null, m.put(e.key, e.value))
     assertTrue(expected == m)
-    assertFalse(m.entries.add(e))
+    assertEquals(e.value, m.put(e.key, e.value))
     assertTrue(expected == m)
 }
 
