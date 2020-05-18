268487c98c8a3ab75b914ba55a6cfa8cf1e1c0e4
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-05-18 19:26:30 +0700

    [Interop][Tests] Add test for CEnumVar.value property

---
+++ backend.native/tests/interop/basics/enums.kt
@@ -0,0 +1,11 @@
+import cenums.*
+import kotlinx.cinterop.*
+import kotlin.test.*
+
+fun main() {
+    memScoped {
+        val e = alloc<E.Var>()
+        e.value = E.C
+        assertEquals(E.C, e.value)
+    }
+}
