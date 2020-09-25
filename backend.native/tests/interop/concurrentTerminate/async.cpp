deb3cbcc4cb1070748181134cd6f18646e5c645d
Vladimir Ivanov <vladimir.d.ivanov@jetbrains.com>
2020-09-25 18:57:08 +0300

    [test] fix and workaround for windows and linux

--- backend.native/tests/interop/concurrentTerminate/async.cpp
+++ backend.native/tests/interop/concurrentTerminate/async.cpp
@@ -7,9 +7,13 @@
 #include "async.h"
 
 int test_ConcurrentTerminate() {
-    signal(SIGABRT, *[](int){ exit(0); }); // Windows does not have sigaction
+    signal(SIGABRT, *[](int){ exit(99); }); // Windows does not have sigaction
 
     std::vector<std::future<void>> futures;
+#ifdef __linux__
+    // TODO: invalid terminate handler called from bridge on non-main thread on Linux X64
+    throw std::runtime_error("Reporting error!");
+#endif
 
     for (size_t i = 0; i < 100; ++i) {
         futures.emplace_back(std::async(std::launch::async,
