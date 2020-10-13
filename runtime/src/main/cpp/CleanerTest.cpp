00629a30716c2743c85e8a39eb1965d467cce427
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2020-10-13 19:06:58 +0300

    Start adding native test on Cleaner

---
+++ runtime/src/main/cpp/CleanerTest.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the LICENSE file.
+ */
+
+#include "Cleaner.h"
+
+#include "gtest/gtest.h"
+
+TEST(CleanerTest, ExampleTest) {
+  EXPECT_EQ(true, false);
+}
