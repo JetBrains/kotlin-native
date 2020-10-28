542a974d489d7f096f24d893d8d80bef6afdb4e2
Sergey Bogolepov <sergey.bogolepov@jetbrains.com>
2020-10-28 16:37:28 +0700

    Fix IrDelegatingConstructorCallImpl.fromSymbolDescriptor usage.

--- backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ir/interop/cenum/CEnumVarClassGenerator.kt
+++ backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/ir/interop/cenum/CEnumVarClassGenerator.kt
@@ -58,9 +58,7 @@ internal class CEnumVarClassGenerator(
         )
         irConstructor.body = irBuilder(irBuiltIns, irConstructor.symbol, SYNTHETIC_OFFSET, SYNTHETIC_OFFSET).irBlockBody {
             +IrDelegatingConstructorCallImpl.fromSymbolDescriptor(
-                    startOffset, endOffset, context.irBuiltIns.unitType, enumVarConstructorSymbol,
-                    enumVarConstructorSymbol.owner.typeParameters.size,
-                    enumVarConstructorSymbol.owner.valueParameters.size
+                    startOffset, endOffset, context.irBuiltIns.unitType, enumVarConstructorSymbol
             ).also {
                 it.putValueArgument(0, irGet(irConstructor.valueParameters[0]))
             }
@@ -83,9 +81,7 @@ internal class CEnumVarClassGenerator(
             it.body = irBuilder(irBuiltIns, it.symbol, SYNTHETIC_OFFSET, SYNTHETIC_OFFSET).irBlockBody {
                 +IrDelegatingConstructorCallImpl.fromSymbolDescriptor(
                         startOffset, endOffset, context.irBuiltIns.unitType,
-                        superConstructorSymbol,
-                        superConstructorSymbol.owner.typeParameters.size,
-                        superConstructorSymbol.owner.valueParameters.size
+                        superConstructorSymbol
                 ).also {
                     it.putValueArgument(0, irInt(typeSize))
                 }
