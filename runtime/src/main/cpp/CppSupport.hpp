dc5a83369711dd0ec7d5eb24d5d860d1b2060b39
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2020-11-19 12:44:30 +0300

    Fix make_unique

--- runtime/src/main/cpp/CppSupport.hpp
+++ runtime/src/main/cpp/CppSupport.hpp
@@ -17,8 +17,8 @@ namespace std_support {
 ////////////////////////// C++14 //////////////////////////
 
 template <typename T, typename... Args>
-std::unique_ptr<T> make_unique(Args... args) {
-    return std::unique_ptr<T>(new T(args...));
+std::unique_ptr<T> make_unique(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 template <typename T>
